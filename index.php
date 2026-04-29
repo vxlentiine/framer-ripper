ac82c5bd20cc90338f5428444fa1cda903596eb7
Felix Hebgen <mail@felixhebgen.de>
2026-04-30 00:14:04 +0200

    Remove direct-origin subdomain routing from index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 require __DIR__ . '/config.php';
 
-$path           = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$host           = $_SERVER['HTTP_HOST'] ?? '';
-$firstSubdomain = explode('.', $host)[0];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($firstSubdomain === 'direct-origin') {
-	require __DIR__ . '/asset.php';
-} elseif ($path === '/robots.txt') {
+if ($path === '/robots.txt') {
     require __DIR__ . '/robots.php';
 } elseif ($path === '/sitemap.xml') {
     require __DIR__ . '/sitemap.php';
